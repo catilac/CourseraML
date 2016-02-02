3691287cc2703356d067c43dc9b111f5e1b9648d
Chirag Davé <c@chirag.io>
2016-02-02 09:55:15 -0800

    completed ex3

--- machine-learning-ex3/ex3/oneVsAll.m
+++ machine-learning-ex3/ex3/oneVsAll.m
@@ -8,8 +8,8 @@ function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 %   to the classifier for label i
 
 % Some useful variables
-m = size(X, 1);
-n = size(X, 2);
+m = size(X, 1); % num rows
+n = size(X, 2); % num cols
 
 % You need to return the following variables correctly 
 all_theta = zeros(num_labels, n + 1);
@@ -36,18 +36,21 @@ X = [ones(m, 1) X];
 %
 % Example Code for fmincg:
 %
-%     % Set Initial theta
-%     initial_theta = zeros(n + 1, 1);
-%     
-%     % Set options for fminunc
-%     options = optimset('GradObj', 'on', 'MaxIter', 50);
-% 
-%     % Run fmincg to obtain the optimal theta
-%     % This function will return theta and the cost 
-%     [theta] = ...
-%         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
-%                 initial_theta, options);
-%
+  for c = 1:num_labels,
+     % Set Initial theta
+     initial_theta = zeros(n + 1, 1);
+     
+     % Set options for fminunc
+     options = optimset('GradObj', 'on', 'MaxIter', 50);
+ 
+     % Run fmincg to obtain the optimal theta
+     % This function will return theta and the cost 
+     [theta] = ...
+         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+                 initial_theta, options);
+     all_theta(c, :) = theta;
+  end
+
 
 
 
